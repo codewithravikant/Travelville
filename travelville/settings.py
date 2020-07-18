8f33e07df647de9f3cb9d4d35acbbf93e80722e8
Ravikant Pandit <54266142+codewithravikant@users.noreply.github.com>
2020-07-18 11:21:15 +0300

    Update settings.py

--- travelville/settings.py
+++ travelville/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vv!287lmimxmp9k-f&b_x_6*64dhfn&(x@)e+m)0tmr0984y(m'
+SECRET_KEY = '###########'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
